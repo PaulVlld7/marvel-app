2a0a606fc8a9338a438b2882c2abd59428188218
Paul Villard <paul.villard@etu.univ-poitiers.fr>
2023-11-07 17:28:16 +0100

    Add build workflow

--- src/pages/CharacterDetailPage.js
+++ src/pages/CharacterDetailPage.js
@@ -7,9 +7,7 @@ const CharacterDetailPage = () => {
     // retrieve the character using the useLoaderData hook
     const character = useLoaderData();
 
-    useEffect(() => {
-        document.title = `${character.name} | Marvel App`;
-    }, [character]);
+    document.title = `${character.name} | Marvel App`;
 
     return (
         <>
